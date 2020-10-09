ca93172821f03365eda14effea00687543b2726f
Eduardo Ismael Garcìa Pèrez <eduardo78d@gmail.com>
2020-10-08 21:09:01 -0500

    filter date

--- image_project/settings.py
+++ image_project/settings.py
@@ -104,7 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ES'
 
 TIME_ZONE = 'UTC'
 
